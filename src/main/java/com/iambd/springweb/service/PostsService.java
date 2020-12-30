ca4bd26efac0bf60bb9bb2373ec348c85837adfc
bangddong <gusehdqkd@naver.com>
2020-12-30 22:43:09 +0900

    4일차  - 티스토리 스킨 참고해서 템플릿 변경  - live reload 추가  - logo 추가

--- src/main/java/com/iambd/springweb/service/PostsService.java
+++ src/main/java/com/iambd/springweb/service/PostsService.java
@@ -17,8 +17,8 @@ public class PostsService {
     private PostsRepository postsRepository;
 
     @Transactional // 트랜잰션, 로직 실행 중 에러 발생시 모든 작업 롤백, 즉 커밋을 하지 않음.
-    public Long save(PostsSaveRequestDto dto) {
-        return postsRepository.save(dto.toEntity()).getId();
+    public void save(PostsSaveRequestDto dto) {
+        postsRepository.save(dto.toEntity()).getId();
     }
 
     @Transactional(readOnly = true)
