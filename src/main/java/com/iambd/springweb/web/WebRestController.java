dd88aac97a63df04a87b5b74fa5b032ed10fd5f2
bangddong <gusehdqkd@naver.com>
2021-01-08 07:50:02 +0900

    13일차  - commons-io, gson 라이브러리 추가  - 썸머노트 게시글 업로드 구현중(특히 사진...)  - 업로드 구현 완료시 썸네일 변환 예정

--- src/main/java/com/iambd/springweb/web/WebRestController.java
+++ src/main/java/com/iambd/springweb/web/WebRestController.java
@@ -1,14 +1,18 @@
 package com.iambd.springweb.web;
 
+import com.google.gson.JsonObject;
 import com.iambd.springweb.domain.posts.PostsRepository;
 import com.iambd.springweb.dto.posts.PostsSaveRequestDto;
 import lombok.AllArgsConstructor;
+import org.apache.commons.io.FileUtils;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.swing.filechooser.FileSystemView;
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
+import java.util.UUID;
 
 @RestController
 @AllArgsConstructor // 모든 필드의 인자값으로 하는 생성자 어노테이션
@@ -45,6 +49,35 @@ public class WebRestController {
         }
 
         return returnString;
+    }
+
+    @PostMapping(value="/uploadSummernoteImageFile", produces = "application/json")
+    @ResponseBody
+    public JsonObject uploadSummernoteImageFile(@RequestParam("file") MultipartFile multipartFile) {
+
+        JsonObject jsonObject = new JsonObject();
+
+        String fileRoot = "C:\\summernote_image\\";	//저장될 외부 파일 경로
+        String originalFileName = multipartFile.getOriginalFilename();	//오리지날 파일명
+        String extension = originalFileName.substring(originalFileName.lastIndexOf("."));	//파일 확장자
+
+        String savedFileName = UUID.randomUUID() + extension;	//저장될 파일 명
+
+        File targetFile = new File(fileRoot + savedFileName);
+
+        try {
+            InputStream fileStream = multipartFile.getInputStream();
+            FileUtils.copyInputStreamToFile(fileStream, targetFile);	//파일 저장
+            jsonObject.addProperty("url", "/summernoteImage/"+savedFileName);
+            jsonObject.addProperty("responseCode", "success");
+
+        } catch (IOException e) {
+            FileUtils.deleteQuietly(targetFile);	//저장된 파일 삭제
+            jsonObject.addProperty("responseCode", "error");
+            e.printStackTrace();
+        }
+
+        return jsonObject;
     }
 
 }
